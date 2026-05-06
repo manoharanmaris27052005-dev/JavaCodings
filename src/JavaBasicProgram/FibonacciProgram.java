99298dcae57c674749b335d309aaec042cbd274c
Maris M <manoharanmaris27052005@gmail.com>
2026-05-06 17:31:44 +0530

    Update FibonacciProgram.java

--- src/JavaBasicProgram/FibonacciProgram.java
+++ src/JavaBasicProgram/FibonacciProgram.java
@@ -1,1 +1,31 @@
 
+package JavaBasicProgram;
+import java.util.Scanner;
+
+public class FibonacciProgram {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in) ;
+			
+		while(true) {
+			System.out.println(" Enter Your Number");
+			int num=sc.nextInt();
+			
+			int a=0;
+			int b=1;
+			for(int i=1;i<=num;i++) {
+				System.out.println(a);
+				int c=a+b;
+				a=b;
+				b=c;
+//				System.out.println(a);
+				
+			}
+			
+		}
+	
+
+	}
+
+} 
